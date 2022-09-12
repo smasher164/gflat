df082ee4c362d70568276a0ee58cedc34d9441ae
Akhil Indurti <aindurti@gmail.com>
2022-09-11 22:05:10 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Gflat programming language";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let supportedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+    ]; in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go_1_18
+            pkgs.gopls
+            pkgs.go-outline
+            pkgs.go-tools
+            pkgs.gopkgs
+            pkgs.delve
+          ];
+        };
+      });
+}
